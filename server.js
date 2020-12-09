85df65b76a0df84b25ce17540e812ded7052a1fa
unknown <parhamwolf051@gmail.com>
2020-12-10 00:07:41 +0330

    start project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello!');
+});
+
+app.listen(3000, () => {
+    console.log('Hi!');
+});
