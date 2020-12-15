8647be93cc5f3735465c2e72efe12e4fb4ed598a
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-15 18:00:51 +0330

    refactor: extract navitems to its own file ♻️

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
+
 import classes from './Navbar.module.css';
+
 import Navitem from './Navitem/Navitem';
 import MainLogo from '../../assets/icons/mainLogo.png';
 import NavDropdown from './NavDropdown/NavDropdown';
 
+import navItems from './navItems';
+
 const Navbar = () => {
-  const [navs, setNavs] = useState([
-    { name: 'سازندگان', items: ['پادکست ها', 'فلان'] },
-    { name: 'منابع', items: ['پادکست ها۲', 'فلان'] },
-    { name: 'قیمت گذاری', items: ['پادکست ها۳', 'فلان'] },
-    { name: 'محصول', items: ['پادکست ها۴', 'فلان'] },
-  ]);
-  const [openNavName, setOpenNavName] = useState('سازندگان');
+  const [openNavName, setOpenNavName] = useState('');
+  const navItemClickHandler = (event, name) => {
+    setOpenNavName(name);
+  };
   return (
     <nav className={classes.navbar}>
       <img className={classes['nav-logo']} src={MainLogo} alt="MainLogo" />
       <div className={classes['nav-items']}>
-        {navs.map(({ name, items }) => {
+        {navItems.map(({ name, items }) => {
           const isOpen = name === openNavName;
           return (
-            <Navitem key={name} isOpen={isOpen}>
+            <Navitem
+              key={name}
+              isOpen={isOpen}
+              onClick={(e) => navItemClickHandler(e, name)}
+            >
               <p>{name}</p>
               <NavDropdown items={items} isOpen={isOpen} />
             </Navitem>
