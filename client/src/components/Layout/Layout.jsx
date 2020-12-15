cd13b00a7b4b8155c0b5ce4c59019e76bf79fe85
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-15 14:28:00 +0330

    feat: include navbar in the layout ✨

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 import classes from './Layout.module.css';
 
 const Layout = ({ children }) => {
-  return <main className={classes.container}>{children}</main>;
+  return (
+    <main className={classes.container}>
+      <Navbar />
+      {children}
+    </main>
+  );
 };
 
 export default Layout;
