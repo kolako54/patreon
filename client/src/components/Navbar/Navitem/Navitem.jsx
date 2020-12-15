56033b40b12a99fe91d8d0c4a9efa6e5c9ae97ef
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-15 18:02:33 +0330

    feat: listen to navitem click event ✨

--- client/src/components/Navbar/Navitem/Navitem.jsx
+++ client/src/components/Navbar/Navitem/Navitem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import classes from './Navitem.module.css';
 
-const Navitem = ({ children, isOpen }) => {
+const Navitem = ({ children, isOpen, onClick }) => {
   const isOpenClassname = isOpen ? classes['nav-item--open'] : '';
 
   return (
-    <div className={`${classes['nav-item']} ${isOpenClassname}`}>
+    <div
+      className={`${classes['nav-item']} ${isOpenClassname}`}
+      onClick={onClick}
+    >
       {children}
     </div>
   );
