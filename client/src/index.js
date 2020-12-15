267e7647dad6eb4601e6e5cfd8884ba938c8cf27
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-15 14:26:34 +0330

    refactor: add root style ♻️

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './root.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
