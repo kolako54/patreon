956744b1224c7545aced78a7bb53c96d1ddc8f10
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-16 23:32:13 +0330

    feat: add routing ✨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import Layout from './components/Layout/Layout';
 import React from 'react';
-
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Landing from './pages/Landing/Landing';
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Router>
+        <Switch>
+          <Route component={Landing} path="/" exact={true} />
+        </Switch>
+      </Router>
+    </Layout>
+  );
 }
 
 export default App;
