236738748253f5957a79acfb8b372e66f6946bce
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-17 00:14:22 +0330

    feat: add dummy video placeholder ✨

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import classes from './Landing.module.css';
+import dummyVideoPlaceholder from '../../assets/dummy/dummyVideoPlaceholder.png';
 
 const Landing = () => {
-  return <div></div>;
+  return (
+    <div className={classes.landing}>
+      <section className={classes['landing__top-content']}>
+        <div className={classes['dummy-video-image']}>
+          <img src={dummyVideoPlaceholder} alt="dummyVideoPlaceholder" />
+        </div>
+      </section>
+      <section className={classes['landing__middle-content']}></section>
+      <footer className={classes['landing__footer']}></footer>
+    </div>
+  );
 };
 
 export default Landing;
