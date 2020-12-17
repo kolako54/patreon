ce850e0fcddd1ed97f35db8d56a2a9cad3435387
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-17 17:26:20 +0330

    feat: add board rightside content ✨

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import classes from './Landing.module.css';
-import dummyVideoPlaceholder from '../../assets/dummy/dummyVideoPlaceholder.png';
+import dummyVideoPlaceholder from '../../assets/images/dummy/dummyVideoPlaceholder.png';
+import Button from '../../components/Button/Button';
 
 const Landing = () => {
   return (
     <div className={classes.landing}>
-      <section className={classes['landing__top-content']}>
-        <div className={classes['dummy-video-image']}>
-          <img src={dummyVideoPlaceholder} alt="dummyVideoPlaceholder" />
+      <section className={classes['top-content']}>
+        <div className={classes.left}>
+          <figure className={classes['dummy-video-image']}>
+            <img src={dummyVideoPlaceholder} alt="dummyVideoPlaceholder" />
+          </figure>
+        </div>
+        <div className={classes.right}>
+          <div className={classes.board}>
+            <p>راه ارزش گذاری هنر را تغییر بده و کسب درامد کن!</p>
+          </div>
+          <Button>شروع کن</Button>
         </div>
       </section>
-      <section className={classes['landing__middle-content']}></section>
-      <footer className={classes['landing__footer']}></footer>
+      <section className={classes['middle-content']}></section>
+      <footer className={classes['footer']}></footer>
     </div>
   );
 };
