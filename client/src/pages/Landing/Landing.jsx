f06c391e4631806e0029f09c6f33fe6799e506f2
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-19 12:48:59 +0330

    feat: add about-patreon ✨

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -22,7 +22,7 @@ const Landing = () => {
         </div>
       </section>
       <section className={classes['middle-content']}>
-        <h1 className={classes['middle-content__header']}>
+        <h1 className={classes['middle-content__heading']}>
           میان <ImportantPart>یک میلیون </ImportantPart>
           نفر پاترئونی جستجو کنید
         </h1>
@@ -35,6 +35,18 @@ const Landing = () => {
             </div>
           </div>
         </form>
+        <div className={classes['about-patreon']}>
+          <h1 className={classes['about-patreon__heading']}>پاترئون چیست</h1>
+          <p>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
+            و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای
+            زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و
+            متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان
+            رایانه ای
+          </p>
+        </div>
       </section>
       <footer className={classes['footer']}></footer>
     </div>
