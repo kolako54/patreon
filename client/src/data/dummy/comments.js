860011f98bd2ec1ceba838a8c12dc1d8d2a5e17d
AmirabbasJ <amirabbasgamer2@gmail.com>
2020-12-23 22:55:37 +0330

    feat: add dummy comment data ✨

---
+++ client/src/data/dummy/comments.js
@@ -0,0 +1,37 @@
+import comment1Image from '../../assets/images/dummy/comments/comment1.jpg';
+import comment2Image from '../../assets/images/dummy/comments/comment2.jpg';
+import comment3Image from '../../assets/images/dummy/comments/comment3.jpg';
+import comment4Image from '../../assets/images/dummy/comments/comment4.jpg';
+
+const comments = [
+  {
+    imgSrc: comment1Image,
+    title: 'یک جریان درامد مکرر ایجاد کنید',
+    textBody:
+      'درآمد قابل اعتماد ماهانه این مکان را برای ایجاد کرده تا بتوانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای خلق کردن صرف کنم',
+    author: { name: 'سمیارا قاسمی', job: 'نویسنده' },
+  },
+  {
+    imgSrc: comment2Image,
+    title: 'یک جریان درامد مکرر ایجاد کنید',
+    textBody:
+      'درآمد قابل اعتماد ماهانه این مکان را برای ایجاد کرده تا بتوانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای خلق کردن صرف کنم',
+    author: { name: 'سمیارا قاسمی', job: 'نویسنده' },
+  },
+  {
+    imgSrc: comment3Image,
+    title: 'یک جریان درامد مکرر ایجاد کنید',
+    textBody:
+      'درآمد قابل اعتماد ماهانه این مکان را برای ایجاد کرده تا بتوانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای خلق کردن صرف کنم',
+    author: { name: 'سمیارا قاسمی', job: 'نویسنده' },
+  },
+  {
+    imgSrc: comment4Image,
+    title: 'یک جریان درامد مکرر ایجاد کنید',
+    textBody:
+      'درآمد قابل اعتماد ماهانه این مکان را برای ایجاد کرده تا بتوانم زمان کمتری را برای تعقیب چک و زمان بیشتری را برای خلق کردن صرف کنم',
+    author: { name: 'سمیارا قاسمی', job: 'نویسنده' },
+  },
+];
+
+export default comments;
