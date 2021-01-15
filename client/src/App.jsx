221a726dd953574a0278b8cb576cbc21c2362ebb
AmirabbasJ <amirabbasgamer2@gmail.com>
2021-01-15 13:36:18 +0330

    feat: render register route ✨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@ import Layout from './components/Layout/Layout';
 import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
+import Register from './pages/Register/Register';
+
 function App() {
   return (
     <Layout>
       <Router>
         <Switch>
           <Route component={Landing} path="/" exact={true} />
+          <Route component={Register} path="/register" />
         </Switch>
       </Router>
     </Layout>
