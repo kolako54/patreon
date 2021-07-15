d7946be71f971168915c5f7b3b72ab19098cd11d
Erfan Ansari <erfanansarimehr.47@gmail.com>
2021-07-15 18:28:45 +0330

    first commit

--- client/src/pages/Landing/Offer/OfferItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './OfferItem.module.css';
-
-const OfferItem = ({ children }) => {
-  return <li className={classes['offer-item']}>{children}</li>;
-};
-
-export default OfferItem;
--- client/src/pages/Landing/Offer/OfferItems.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './OfferItems.module.css';
-
-const OfferItems = ({ children }) => {
-  return <ul className={classes['offer-items']}>{children}</ul>;
-};
-
-export default OfferItems;
