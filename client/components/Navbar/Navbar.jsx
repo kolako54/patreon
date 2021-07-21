8087223d096dac5e6c551493e81eabf98be3338a
Erfan Ansari <erfanansarimehr.47@gmail.com>
2021-07-21 16:35:02 +0330

    refactor navbar

--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -1,18 +1,33 @@
 import Link from 'next/link'
 import Image from "next/image";
 import {useState} from "react";
-import Logo from 'public/favicon.png'
+import logo from 'public/favicon.png'
 import styles from './Navbar.module.scss'
 import {IoMenu, IoClose} from 'react-icons/io5'
 
-const links = ['For creators','Pricing','Resources','Starter kits'].map(el => (
+const links = ['For creators', 'Pricing', 'Resources', 'Starter kits'].map(el => (
     <Link key={el} href={el.toLowerCase()}>
         <a className={styles.link}>
             {el}
         </a>
     </Link>
 ))
+const favLogo = <Image width={50} height={50} src={logo} alt="logo"/>
 
+const registerLinks = (
+    <div className={styles.navButtons}>
+        <Link href="/login">
+            <a className={styles.logIn}>
+                Log In
+            </a>
+        </Link>
+        <Link href="/signin">
+            <a className={styles.signIn}>
+                Sign In
+            </a>
+        </Link>
+    </div>
+)
 export default function Navbar() {
     const [open, setOpen] = useState(false)
     const handleMenu = () => {
@@ -23,60 +38,17 @@ export default function Navbar() {
             <div className={styles.container}>
                 <div className={styles.navbar}>
                     <div className={styles.navLinks}>
-                        <Image width={50} height={50} src={Logo} alt="logo"/>
-                        {/*<Link href="/product">*/}
-                        {/*    <a className={styles.link}>*/}
-                        {/*        For creators*/}
-                        {/*    </a>*/}
-                        {/*</Link>*/}
-                        {/*<Link href="/pricing">*/}
-                        {/*    <a className={styles.link}>*/}
-                        {/*        pricing*/}
-                        {/*    </a>*/}
-                        {/*</Link>*/}
-                        {/*<Link href="/resources">*/}
-                        {/*    <a className={styles.link}>*/}
-                        {/*        Resources*/}
-                        {/*    </a>*/}
-                        {/*</Link>*/}
-                        {/*<Link href="/starter">*/}
-                        {/*    <a className={styles.link}>*/}
-                        {/*        Starter kits*/}
-                        {/*    </a>*/}
-                        {/*</Link>*/}
+                        {favLogo}
                         {links}
                     </div>
-
-                    <div className={styles.navButtons}>
-                        <Link href="/login">
-                            <a className={styles.logIn}>
-                                Log In
-                            </a>
-                        </Link>
-                        <Link href="/signin">
-                            <a className={styles.signIn}>
-                                Sign In
-                            </a>
-                        </Link>
-                    </div>
+                    {registerLinks}
                 </div>
 
                 <div className={styles.buttons}>
                     <div style={{marginRight: 'auto'}}>
-                        <Image width={50} height={50} src={Logo} alt="logo"/>
+                        {favLogo}
                     </div>
-                    <div className={styles.navButtons}>
-                        <Link href="/login">
-                            <a className={styles.logIn}>
-                                Log In
-                            </a>
-                        </Link>
-                        <Link href="/signin">
-                            <a className={styles.signIn}>
-                                Sign In
-                            </a>
-                        </Link>
-                    </div>
+                    {registerLinks}
                     {!open ?
                         <button onClick={handleMenu}>
                             <IoMenu color="#fff" size={40}/>
@@ -86,9 +58,7 @@ export default function Navbar() {
                             <IoClose color="#fff" size={40}/>
                         </button>
                     }
-
                 </div>
-
             </div>
             {open &&
             <div className={styles.subMenu}>
