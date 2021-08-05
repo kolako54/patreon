578b78829d2c6d362a4798b973ce860b01429ac7
Erfan Ansari <erfanansarimehr.47@gmail.com>
2021-08-05 23:00:39 +0330

    added nprogress

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,17 +1,37 @@
 import {ApolloProvider} from "@apollo/client";
 import {useApollo} from "$apollo/apolloClient"
 import {Provider} from "next-auth/client";
+import NProgress from 'nprogress'
+import Head from 'next/head'
+import Router from "next/router";
 import '$styles/globals.css'
 import 'swiper/swiper.scss'
+import '$styles/nprogress.css'
+
+Router.onRouteChangeStart = url => {
+    NProgress.start()
+}
+
+Router.onRouteChangeComplete = () => NProgress.done()
+
+Router.onRouteChangeError = () => NProgress.done()
+
 
 function MyApp({Component, pageProps}) {
     const client = useApollo(pageProps.initialApolloState);
-    return (
-        <Provider session={pageProps.session}>
-            <ApolloProvider client={client}>
-                <Component {...pageProps} />
-            </ApolloProvider>
-        </Provider>
+    return (<>
+            {/*default title is set to Patreon in Layout page*/}
+            <Head>
+                <link
+                    rel="stylesheet"
+                />
+            </Head>
+            <Provider session={pageProps.session}>
+                <ApolloProvider client={client}>
+                    <Component {...pageProps} />
+                </ApolloProvider>
+            </Provider>
+        </>
     )
 }
 
