e85acd8eeb7172a89f4e2203b1b6cbb0063c21a3
Erfan Ansari <erfanansarimehr.47@gmail.com>
2021-09-25 23:57:55 +0330

    deleted package-lock and .idea

--- client/pages/api/queries.js
+++ client/pages/api/queries.js
@@ -1,41 +1,55 @@
-import { gql } from '@apollo/client'
+import { gql } from "@apollo/client"
 
-export const REGISTER = gql`mutation SIGN_UP($name: String! $email: String! $password: String! $confirmPassword: String! $profile_pic: String ){
-    signUp(UserInput: { name: $name email: $email password: $password confirmPassword: $confirmPassword profile_pic: $profile_pic}){
-      user{
-      id
-      name
-      email
-      profile_pic
+export const REGISTER = gql`
+    mutation SIGN_UP($name: String!, $email: String!, $password: String!, $confirmPassword: String!, $profile_pic: String) {
+        signUp(UserInput: { name: $name, email: $email, password: $password, confirmPassword: $confirmPassword, profile_pic: $profile_pic }) {
+            user {
+                id
+                name
+                email
+                profile_pic
+            }
+        }
     }
-      token
-    }
-  }`
+`
 
-
-export const GET_USER = gql`
-query LOGIN($email: String! $password: String!) {
-  loginUser(UserLoginInput: { email: $email password: $password}) {
-    user {
-      email
+export const GET_ME = gql`
+    query Get_Me {
+        get_me {
+            id
+            name
+            email
+            profile_pic
+        }
     }
-    token
-  }
-}`
-export const GET_ME = gql` 
-query Get_Me {
-  get_me {
-    id
-    name
-    email
-    profile_pic
-  }
-}`
+`
 
 export const UPDATE_PASSWORD = gql`
-mutation UPDATE_PASSWORD($currentPassword: String!, $password: String!, $confirmPassword: String!) {
-  updatePassword(currentPassword: $currentPassword, password: $password, confirmPassword: $confirmPassword) {
-    token
-  }
-}
+    mutation UPDATE_PASSWORD($currentPassword: String!, $password: String!, $confirmPassword: String!) {
+        updatePassword(currentPassword: $currentPassword, password: $password, confirmPassword: $confirmPassword) {
+            user {
+                id
+                name
+                email
+                profile_pic
+            }
+        }
+    }
+`
+export const LOGIN_USER = gql`
+    mutation LOGIN($email: String!, $password: String!) {
+        loginUser(UserLoginInput: { email: $email, password: $password }) {
+            user {
+                email
+                id
+                name
+                profile_pic
+            }
+        }
+    }
+`
+export const REFRESH = gql`
+    query REFRESH($id: ID!) {
+        refresh(id: $id)
+    }
 `
